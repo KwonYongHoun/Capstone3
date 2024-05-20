4ecaa7bfb3affdf58edb48e21dc4a61d8689519e
JAENSHIN <aiaiai200205@gmail.com>
2024-05-20 17:49:28 +0900

    커뮤니티, 상세페이지 및 게시판 별 목록 구현

--- lib/Sin/Commu.dart
+++ lib/Sin/Commu.dart
@@ -5,15 +5,25 @@ class Commu {
   final int? postID;
   final int? fk_memberNumber;
   final String type;
+  final String title;
   final String content;
   final DateTime createdAt;
+  int? commentCount;
+  int? likeCount;
+  DateTime? timestamp;
+  String? name;
 
   Commu({
     this.postID,
     this.fk_memberNumber,
     required this.type,
+    required this.title,
     required this.content,
     required this.createdAt,
+    this.commentCount,
+    this.likeCount,
+    this.timestamp,
+    this.name,
   });
 
   Map<String, dynamic> toMap() {
@@ -21,6 +31,35 @@ class Commu {
       'postID': postID,
       'fk_memberNumber': fk_memberNumber,
       'type': type,
+      'title': title,
+      'content': content,
+      'createdAt': createdAt.toIso8601String(),
+      'name': name,
+      'likeCount': likeCount,
+    };
+  }
+}
+
+class Comment {
+  final int? commentID;
+  final int postID;
+  final int memberNumber;
+  final String content;
+  final DateTime createdAt;
+
+  Comment({
+    this.commentID,
+    required this.postID,
+    required this.memberNumber,
+    required this.content,
+    required this.createdAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'commentID': commentID,
+      'postID': postID,
+      'memberNumber': memberNumber,
       'content': content,
       'createdAt': createdAt.toIso8601String(),
     };
@@ -29,76 +68,196 @@ class Commu {
 
 class DatabaseHelper {
   static late Database _database;
-  static const String dbName = 'commu.db';
-  static const String tableName = 'Posts';
+  static const String commuDbName = 'commu.db';
+  static const String postsTable = 'Posts';
+  static const String commentsTable = 'Comments';
 
   static Future<void> initDatabase() async {
     _database = await openDatabase(
-      join(await getDatabasesPath(), dbName),
+      join(await getDatabasesPath(), commuDbName),
       onCreate: (db, version) async {
-        await db.execute('''
-          CREATE TABLE $tableName(
-            postID INTEGER PRIMARY KEY,
-            fk_memberNumber INTEGER,
-            type TEXT,
-            content TEXT,
-            createdAt TEXT
-          )
-        ''');
-
-        // 데이터베이스가 생성될 때 게시물 데이터를 넣습니다.
+        await _createTables(db);
         await _insertInitialData(db);
       },
-      version: 1,
+      version: 2,
+      onUpgrade: (db, oldVersion, newVersion) async {
+        if (oldVersion < 2) {
+          await _createTables(db);
+        }
+      },
     );
   }
 
+  static Future<void> _createTables(Database db) async {
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS $postsTable(
+        postID INTEGER PRIMARY KEY,
+        fk_memberNumber INTEGER,
+        type TEXT,
+        title TEXT, -- 제목 필드 추가
+        content TEXT,
+        createdAt TEXT,
+        name TEXT,
+        likeCount INTEGER DEFAULT 0
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS $commentsTable(
+        commentID INTEGER PRIMARY KEY,
+        postID INTEGER,
+        memberNumber INTEGER,
+        content TEXT,
+        createdAt TEXT,
+        FOREIGN KEY (postID) REFERENCES $postsTable(postID)
+      )
+    ''');
+  }
+
   static Future<void> _insertInitialData(Database db) async {
-    List<Commu> initialPosts = [
-      Commu(
-        postID: 1,
-        fk_memberNumber: 1,
-        type: '자유게시판',
-        content: '첫 번째 게시물 내용입니다.',
-        createdAt: DateTime.now(),
-      ),
-      Commu(
-        postID: 2,
-        fk_memberNumber: 2,
-        type: '헬스 파트너 찾기',
-        content: '두 번째 게시물 내용입니다.',
-        createdAt: DateTime.now(),
-      ),
-      Commu(
-        postID: 3,
-        fk_memberNumber: 3,
-        type: '운동 고민 게시판',
-        content: '세 번째 게시물 내용입니다.',
-        createdAt: DateTime.now(),
-      ),
+    final List<Map<String, dynamic>> initialData = [
+      {
+        'postID': 1,
+        'fk_memberNumber': 1,
+        'type': '자유게시판',
+        'title': '첫 번째 게시물 제목', // 제목 데이터 추가
+        'content': '첫 번째 게시물 내용입니다.',
+        'createdAt': DateTime.now().toIso8601String(),
+        'name': '회원1',
+      },
+      {
+        'postID': 2,
+        'fk_memberNumber': 2,
+        'type': '헬스 파트너 찾기',
+        'title': '두 번째 게시물 제목', // 제목 데이터 추가
+        'content': '두 번째 게시물 내용입니다.',
+        'createdAt': DateTime.now().toIso8601String(),
+        'name': '회원2',
+      },
+      {
+        'postID': 3,
+        'fk_memberNumber': 3,
+        'type': '운동 고민 게시판',
+        'title': '세 번째 게시물 제목', // 제목 데이터 추가
+        'content': '세 번째 게시물 내용입니다.',
+        'createdAt': DateTime.now().toIso8601String(),
+        'name': '회원3',
+      },
     ];
 
-    for (var post in initialPosts) {
-      await db.insert(tableName, post.toMap());
+    for (final data in initialData) {
+      await db.insert(postsTable, data);
     }
   }
 
-  static Future<void> insertPost(Commu post) async {
-    await _database.insert(tableName, post.toMap());
+  static Future<List<Commu>> getPostsByType(String type) async {
+    final List<Map<String, dynamic>> maps = await _database.query(
+      postsTable,
+      where: 'type = ?',
+      whereArgs: [type],
+    );
+
+    List<Commu> posts = [];
+    for (final map in maps) {
+      final int postID = map['postID'];
+      final int commentCount = await _getCommentCount(postID);
+      posts.add(
+        Commu(
+          postID: map['postID'],
+          fk_memberNumber: map['fk_memberNumber'],
+          type: map['type'],
+          title: map['title'],
+          content: map['content'],
+          createdAt: DateTime.parse(map['createdAt']),
+          commentCount: commentCount,
+          timestamp: DateTime.parse(map['createdAt']),
+          name: map['name'],
+        ),
+      );
+    }
+
+    return posts;
   }
 
-  static Future<List<Commu>> getPosts() async {
-    final List<Map<String, dynamic>> maps = await _database.query(tableName);
+  static Future<void> insertPost(Commu post) async {
+    await _database.insert(postsTable, post.toMap());
+  }
+
+  static Future<void> insertComment(Comment comment) async {
+    await _database.insert(commentsTable, comment.toMap());
+  }
+
+  static Future<List<Comment>> getCommentsByPostID(int postID) async {
+    final List<Map<String, dynamic>> maps = await _database.query(
+      commentsTable,
+      where: 'postID = ?',
+      whereArgs: [postID],
+    );
     return List.generate(maps.length, (i) {
-      return Commu(
+      return Comment(
+        commentID: maps[i]['commentID'],
         postID: maps[i]['postID'],
-        fk_memberNumber: maps[i]['fk_memberNumber'],
-        type: maps[i]['type'],
+        memberNumber: maps[i]['memberNumber'],
         content: maps[i]['content'],
         createdAt: DateTime.parse(maps[i]['createdAt']),
       );
     });
   }
 
-  // 추가적인 메소드들 (예: 삭제, 업데이트)도 필요에 따라 작성 가능
+  static Future<List<Commu>> getPosts() async {
+    final List<Map<String, dynamic>> maps = await _database.query(postsTable);
+
+    List<Commu> posts = [];
+    for (final map in maps) {
+      final int postID = map['postID'];
+      final int commentCount = await _getCommentCount(postID);
+      posts.add(
+        Commu(
+          postID: map['postID'],
+          fk_memberNumber: map['fk_memberNumber'],
+          type: map['type'],
+          title: map['title'],
+          content: map['content'],
+          createdAt: DateTime.parse(map['createdAt']),
+          commentCount: commentCount,
+          timestamp: DateTime.parse(map['createdAt']),
+          name: map['name'], // 이름 추가
+        ),
+      );
+    }
+
+    return posts;
+  }
+
+  static Future<int> _getCommentCount(int postID) async {
+    final List<Map<String, dynamic>> maps = await _database.query(
+      commentsTable,
+      where: 'postID = ?',
+      whereArgs: [postID],
+    );
+    return maps.length;
+  }
+
+  static Future<int> getLikeCount(int postID) async {
+    final List<Map<String, dynamic>> maps = await _database.query(
+      postsTable,
+      columns: ['likeCount'],
+      where: 'postID = ?',
+      whereArgs: [postID],
+    );
+    if (maps.isNotEmpty) {
+      return maps.first['likeCount'];
+    } else {
+      return 0;
+    }
+  }
+
+  static Future<void> updateLikeCount(int postID, int newLikeCount) async {
+    await _database.update(
+      postsTable,
+      {'likeCount': newLikeCount},
+      where: 'postID = ?',
+      whereArgs: [postID],
+    );
+  }
 }
--- lib/Sin/posts_list.dart
+++ lib/Sin/posts_list.dart
@@ -23,7 +23,13 @@ class _PostsListPageState extends State<PostsListPage> {
 
   Future<List<Commu>> _fetchPosts() async {
     await DatabaseHelper.initDatabase();
-    return DatabaseHelper.getPosts();
+    return DatabaseHelper.getPostsByType(widget.boardType);
+  }
+
+  void _refreshPosts() {
+    setState(() {
+      _postsFuture = _fetchPosts();
+    });
   }
 
   @override
@@ -32,41 +38,43 @@ class _PostsListPageState extends State<PostsListPage> {
       appBar: AppBar(
         title: Text(widget.boardType),
       ),
-      body: FutureBuilder<List<Commu>>(
-        future: _postsFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('No posts available.'));
-          } else {
-            final posts = snapshot.data!;
-            return ListView.builder(
-              itemCount: posts.length,
-              itemBuilder: (context, index) {
-                final post = posts[index];
-                return ListTile(
-                  title: Text(post.type),
-                  subtitle: Text(
-                    post.content.length > 50
-                        ? '${post.content.substring(0, 50)}...'
-                        : post.content,
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => PostDetailPage(post: post),
-                      ),
-                    );
-                  },
-                );
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder<List<Commu>>(
+              future: _postsFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Center(child: Text('No posts available.'));
+                } else {
+                  final posts = snapshot.data!;
+                  return ListView.builder(
+                    itemCount: posts.length,
+                    itemBuilder: (context, index) {
+                      final post = posts[index];
+                      return Column(
+                        children: [
+                          PostWidget(
+                            post: post,
+                            onPostUpdated: _refreshPosts,
+                          ),
+                          Divider(
+                            color: Colors.grey, // 선의 색상
+                            height: 10, // 선의 높이
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
               },
-            );
-          }
-        },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -75,10 +83,90 @@ class _PostsListPageState extends State<PostsListPage> {
             MaterialPageRoute(
               builder: (context) => WritePage(), // 글쓰기 페이지로 이동
             ),
-          );
+          ).then((_) {
+            _refreshPosts(); // 글쓰기 페이지에서 돌아오면 목록을 갱신합니다.
+          });
         },
         child: Icon(Icons.create), // 연필 아이콘
-        backgroundColor: Colors.pink, // 분홍색 배경색
+        backgroundColor: Colors.lightBlue,
+      ),
+    );
+  }
+}
+
+class PostWidget extends StatelessWidget {
+  final Commu post;
+  final VoidCallback onPostUpdated;
+
+  PostWidget({required this.post, required this.onPostUpdated});
+
+  @override
+  Widget build(BuildContext context) {
+    String formattedTimestamp =
+        '${post.createdAt.year}-${post.createdAt.month}-${post.createdAt.day} ${post.createdAt.hour}:${post.createdAt.minute}';
+
+    if (post.title == null) {
+      print('Post title is null for postID: ${post.postID}');
+    } else {
+      print('Post title for postID ${post.postID}: ${post.title}');
+    }
+
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => PostDetailPage(
+              post: post,
+              onCommentAdded: onPostUpdated,
+            ),
+          ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.symmetric(vertical: 5),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              post.title ?? 'No Title', // null인 경우 처리
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              post.content,
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.comment),
+                    SizedBox(width: 5),
+                    Text(
+                      '${post.commentCount}',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ],
+                ),
+                Text(
+                  '$formattedTimestamp',
+                  style: TextStyle(fontSize: 12),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }

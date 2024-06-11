94e2a8438d1d45b27aeef339258514aee0a1e690
JAENSHIN <aiaiai200205@gmail.com>
2024-06-11 21:17:32 +0900

    0917

--- lib/Park/homescreen.dart
+++ lib/Park/homescreen.dart
@@ -29,7 +29,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void _startTimer() {
-    _timer = Timer.periodic(Duration(seconds: 10), (timer) {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
       _refreshCongestion();
     });
   }
